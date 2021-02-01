bff4a2f82aa171fc1596575349aa6e44cecd05a1
Suraj Odedra <suraj699@icloud.com>
2021-02-01 19:42:07 +0000

    Tried adding testing and the class

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            
+        }
+
+
+    }
+}
+
