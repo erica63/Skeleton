c08bdca7a196704754b92ce1696dd77b9853410f
Erica Thuet <P2447778@my365.dmu.ac.uk>
2021-02-10 19:32:30 +0000

    Added more test methods to tstOrderProcessing, also updated packages to stable version because tests were not going through.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
--- Testing4/tstOrderProcessing.cs
+++ Testing4/tstOrderProcessing.cs
@@ -15,5 +15,69 @@ namespace Testing4
             //Test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void OrderDeliveryPropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Create some test data to assign to the property
+            Boolean TestData = true;
+            //Assign the data to the property
+            AnOrder.Active = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Active, TestData);
+        }
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //Assign the data to the property
+            AnOrder.DateAdded = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void OrderRatingPropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Create some test data to assign to the property
+            int TestData = 1;
+            //Assign the data to the property
+            AnOrder.Rating = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Rating, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDescriptionPropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Create some test data to assign to the property
+            string TestData = "Fila";
+            //Assign the data to the property
+            AnOrder.Description = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Description, TestData);
+        }
+        [TestMethod]
+        public void OrderPriceOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Create some test data to assign to the property
+            decimal TestData = 23.99m;
+            //Assign the data to the property
+            AnOrder.TotalPrice = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnOrder.TotalPrice, TestData);
+        }
+
     }
 }
