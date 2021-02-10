7ffa05e6e4b9eee4d230b85d2f034492afbebe98
Erica Thuet <P2447778@my365.dmu.ac.uk>
2021-02-10 17:50:39 +0000

    Added testing and class for Order Processing

---
+++ Testing4/tstOrderProcessing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrderProcessing
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //Test to see that it exists
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
