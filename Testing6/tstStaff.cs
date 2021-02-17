b579191acbe2edaf590a06aece6262435a0ae69c
Jay Singh <P2577997@my365.dmu.ac.uk>
2021-02-17 18:19:24 +0000

    Added testing and class for Staff mangement section that i am working on. Had to restart as i was having difficulties with what i was doing on my end however i believe progress should be a bit more swift from here.

---
+++ Testing6/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsStaff StaffMember = new clsStaff();
+            //test to see that it exists 
+            Assert.IsNotNull(StaffMember);
+        }
+    }
+}
