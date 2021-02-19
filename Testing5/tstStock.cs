dde86abb913ab707327d66d3d5ac75d25c6657a2
Harmanjeet Singh <P2535409@my365.dmu.ac.uk>
2021-02-19 09:34:20 +0000

    Added testing and class for Stock Management.

---
+++ Testing5/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
