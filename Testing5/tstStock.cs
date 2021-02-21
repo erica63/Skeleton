4fe159143bc2f729831b9e8bbb33185e68c13bf8
Harmanjeet Singh <P2535409@my365.dmu.ac.uk>
2021-02-21 15:07:59 +0000

    Created properties for my table design (still two left), and tested them.

--- Testing5/tstStock.cs
+++ Testing5/tstStock.cs
@@ -8,12 +8,70 @@ namespace Testing5
     public class tstStock
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
             //create an instance of the class we want to create
             clsStock AStock = new clsStock();
             //test to see that it exists
             Assert.IsNotNull(AStock);
+
+        }
+
+            [TestMethod]
+        public void ItemIdOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.ItemId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ItemId, TestData);
+        }
+
+        [TestMethod]
+        public void ItemNameOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "21b";
+            //assign the data to the property
+            AStock.ItemName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ItemName, TestData);
+        }
+
+        [TestMethod]
+        public void ItemAvailableOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AStock.ItemAvailable = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ItemAvailable, TestData);
+        }
+
+        [TestMethod]
+        public void DateItemAddedOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStock.DateItemAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.DateItemAdded, TestData);
+        }
+
+        
+
+
         }
     }
-}
+
