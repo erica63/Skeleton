5e2ca4c88d2fa52eb91ea9b203b91132c84b4a7a
Suraj Odedra <suraj699@icloud.com>
2021-02-25 07:54:46 +0000

    some test pass

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+
+        //private data member for the list
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        //public property for the address list
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                //set the private data
+                mCustomerList = value;
+            }
+        }
+
+            
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mCustomerList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+
+        public clsCustomer ThisCustomer { get; set; }
+    }
+}
