04aee4b59c65863a5656e149b70a503d5180d254
Erica Thuet <P2447778@my365.dmu.ac.uk>
2021-03-16 14:05:47 +0000

    Fixed my errors and removed redundant NuGet packages

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -6,68 +6,12 @@ namespace ClassLibrary
     public class clsOrderCollection
     {
 
-        public clsOrderCollection()
-        {
+        //Private data member for the list
+        List<clsOrder> mOrderList = new List<clsOrder>();
+        //Private data member thisOrder
+        clsOrder mThisOrder = new clsOrder();
 
-            //Private data member for the list
-            List<clsOrder> mOrderList = new List<clsOrder>();
-            //Private data member thisOrder
-            clsOrder mThisOrder = new clsOrder();
-
-
-            /** //Var for the index
-            Int32 Index = 0;
-            //Var to store the record count
-            Int32 RecordCount = 0;
-            //Object for the data connection
-            clsDataConnection DB = new clsDataConnection();
-            //Execute the stored procedure
-            DB.Execute("sproc_tblOrder_SelectAll");
-            //Get the count of records
-            RecordCount = DB.Count;
-            //While there are records to process
-            while (Index < RecordCount)
-            {
-                //Create a blank order
-                clsOrder AnOrder = new clsOrder();
-                //Read in the fields from the current record
-                AnOrder.Delivery = Convert.ToBoolean(DB.DataTable.Rows[Index]["OrderDelivery"]);
-                AnOrder.OrderId = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderId"]);
-                AnOrder.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
-                AnOrder.ShippingAddress = Convert.ToString(DB.DataTable.Rows[Index]["ShippingAddress"]);
-                AnOrder.Description = Convert.ToString(DB.DataTable.Rows[Index]["OrderDesc"]);
-                AnOrder.OrderQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderQuantity"]);
-                //Add the record to the private data member
-                mOrderList.Add(AnOrder);
-                //Point at the next record
-                Index++;
-            } */
-
-            /** Old code WK 23 - Creating the Collection Class Properties Middle Layer
-            //Create the items of the test data
-            clsOrder TestItem = new clsOrder();
-            //Set its properties
-            TestItem.Delivery = true;
-            TestItem.OrderId = 1;
-            TestItem.OrderQuantity = 1;
-            TestItem.ShippingAddress = "22 New Park St";
-            TestItem.Description = "ADIDAS";
-            TestItem.DateAdded = DateTime.Parse("03/01/2021");
-            //Add the item to the test list
-            mOrderList.Add(TestItem);
-            //Re initialise the object for some new data
-            TestItem = new clsOrder();
-            //Set its properties
-            TestItem.Delivery = true;
-            TestItem.OrderId = 2;
-            TestItem.OrderQuantity = 1;
-            TestItem.ShippingAddress = "91 Cloud St";
-            TestItem.Description = "NIKE AIR";
-            TestItem.DateAdded = DateTime.Parse("10/01/2021");
-            //Add the item to the test list
-            mOrderList.Add(TestItem);*/
-
-
+        //Public property for the order list
         public List<clsOrder> OrderList
         {
             get
@@ -91,7 +35,7 @@ namespace ClassLibrary
             }
             set
             {
-               
+
             }
         }
 
@@ -117,4 +61,4 @@ namespace ClassLibrary
         }
     }
 }
-}
+
